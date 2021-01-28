c84263552b476c759be3d1a677fe292296604b8d
p1t1ch <belobeev.kirill@gmail.com>
2021-01-28 12:17:41 +0300

    write styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,41 +6,74 @@ import Seo from '@/components/Seo'
 
 function IndexPage() {
   return (
-    <main>
+    <main className="min-h-screen grid md:grid-cols-container items-center place-content-center gap-11 py-22 px-6 bg-mobile md:bg-desktop bg-no-repeat bg-cover">
       <Seo title="Frontend Mentor: Intro component with sign-up form" />
-      <h1>Learn to code by watching others</h1>
-      <p>
-        See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but
-        understanding how developers think is invaluable.
-      </p>
-      <div>
-        <span>Try it free 7 days</span> then $20/mo. thereafter
-      </div>
-      <form noValidate>
-        <label htmlFor="firstName" className="sr-only">
-          First Name
-        </label>
-        <input type="text" id="firstName" placeholder="First Name" />
-        <label htmlFor="lastName" className="sr-only">
-          Last Name
-        </label>
-        <input type="text" id="lastName" placeholder="Last Name" />
-        <label htmlFor="emailAddress" className="sr-only">
-          Email Address
-        </label>
-        <input type="email" id="emailAddress" placeholder="Email Address" />
-        <label htmlFor="password" className="sr-only">
-          Password
-        </label>
-        <input type="password" id="password" placeholder="Password" />
-        <button type="submit">Claim your free trial</button>
+      <section className="text-center md:text-left">
+        <h1 className="text-heading-mobile md:text-heading-desktop font-bold mb-6 md:mb-10">
+          Learn to code by watching others
+        </h1>
         <p>
-          By clicking the button, you are agreeing to our{' '}
-          <a href="/" target="_blank" rel="noindex nofollow noreferrer">
-            Terms and Services
-          </a>
+          See how experienced developers solve problems in real-time. Watching scripted tutorials is great, but
+          understanding how developers think is invaluable.
         </p>
-      </form>
+      </section>
+      <section>
+        <div className="bg-purple shadow rounded mb-6 font-normal text-cta p-4 text-center">
+          <span className="font-bold">Try it free 7 days</span> then $20/mo. thereafter
+        </div>
+        <form noValidate className="grid gap-4 md:gap-5 p-6 md:p-10 bg-white shadow rounded">
+          <label htmlFor="firstName" className="sr-only">
+            First Name
+          </label>
+          <input
+            type="text"
+            id="firstName"
+            placeholder="First Name"
+            className="text-input font-semibold h-14 px-8 text-neutral placeholder:text-opacity-75 border border-gray rounded-small"
+          />
+          <label htmlFor="lastName" className="sr-only">
+            Last Name
+          </label>
+          <input
+            type="text"
+            id="lastName"
+            placeholder="Last Name"
+            className="text-input font-semibold h-14 px-8 text-neutral placeholder:text-opacity-75 border border-gray rounded-small"
+          />
+          <label htmlFor="emailAddress" className="sr-only">
+            Email Address
+          </label>
+          <input
+            type="email"
+            id="emailAddress"
+            placeholder="Email Address"
+            className="text-input font-semibold h-14 px-8 text-neutral placeholder:text-opacity-75 border border-gray rounded-small"
+          />
+          <label htmlFor="password" className="sr-only">
+            Password
+          </label>
+          <input
+            type="password"
+            id="password"
+            placeholder="Password"
+            className="text-input font-semibold h-14 px-8 text-neutral placeholder:text-opacity-75 border border-gray rounded-small"
+          />
+          <div>
+            <button
+              type="submit"
+              className="mb-2 h-14 px-8 text-center shadow-inset rounded-small bg-green font-semibold uppercase text-cta w-full"
+            >
+              Claim your free trial
+            </button>
+            <p className="text-light text-note text-center">
+              By clicking the button, you are agreeing to our{' '}
+              <a href="/" target="_blank" rel="noindex nofollow noreferrer" className="font-bold text-red">
+                Terms and Services
+              </a>
+            </p>
+          </div>
+        </form>
+      </section>
     </main>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,38 @@ module.exports = {
       green: '#38CC8B',
       purple: '#5E54A4',
       neutral: '#3D3B48',
+      light: '#BAB7D4',
+      gray: '#DEDEDE',
       white: '#FFF',
     },
     extend: {
       fontFamily: {
         sans: ['Poppins', ...defaultTheme.fontFamily.sans],
+      },
+      fontSize: {
+        'heading-desktop': ['3.125rem', 1.1],
+        'heading-mobile': ['1.75rem', 1.29],
+        note: ['.6875rem', 1.9],
+        cta: ['.9375rem', 1.73],
+        input: ['.875rem', 1.86],
+      },
+      padding: {
+        22: '5.5rem',
+      },
+      gridTemplateColumns: {
+        container: 'minmax(auto, 32.8125rem) minmax(auto, 33.75rem)',
+      },
+      boxShadow: {
+        DEFAULT: '0px 8px 0px rgba(0, 0, 0, 0.14688)',
+        inset: 'inset 0px -4px 0px rgba(0, 0, 0, 0.0908818)',
+      },
+      borderRadius: {
+        DEFAULT: '.625rem',
+        small: '.3125rem',
+      },
+      backgroundImage: {
+        desktop: 'url(../images/bg-intro-desktop.png)',
+        mobile: 'url(../images/bg-intro-mobile.png)',
       },
     },
   },
